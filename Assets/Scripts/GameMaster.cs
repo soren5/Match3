38734882372e6384fb68dd30d6e293d04225dc66
DESKTOP-9ASLHGF\lamec <pedrofilipecarvalho@mac.com>
2017-02-07 16:32:31 +0000

    Initial Commit

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public GameObject RedGem;
+    public GameObject BlueGem;
+    public GameObject YellowGem;
+    public GameObject GreenGem;
+    public GameObject PinkGem;
+    public static float newGemX;
+    public static bool destructionSemaphore;
+
+	// Use this for initialization
+	void Start () {
+        destructionSemaphore = false;
+        newGemX = 0.1f;
+        for (int numCols = 0; numCols < 12; numCols++) {
+            for (int numRows = 0; numRows < 12; numRows++) {
+                int random = Random.Range(1, 6);
+                if(random == 1){
+                    Instantiate(BlueGem, new Vector3((float)(-4.4 + numCols * 0.8), (float)(-4.4 + numRows * 0.8)), new Quaternion());
+                }
+                if (random == 2){
+                    Instantiate(RedGem, new Vector3((float)(-4.4 + numCols * 0.8), (float)(-4.4 + numRows * 0.8)), new Quaternion());
+                }
+                if (random == 3){
+                    Instantiate(GreenGem, new Vector3((float)(-4.4 + numCols * 0.8), (float)(-4.4 + numRows * 0.8)), new Quaternion());
+                }
+                if (random == 4){
+                    Instantiate(YellowGem, new Vector3((float)(-4.4 + numCols * 0.8), (float)(-4.4 + numRows * 0.8)), new Quaternion());
+                }
+                if (random > 4){
+                    Instantiate(PinkGem, new Vector3((float)(-4.4 + numCols * 0.8), (float)(-4.4 + numRows * 0.8)), new Quaternion());
+                }
+            }
+        }
+	}
+
+    static public IEnumerator destroyingBlock()
+    {
+        Debug.Log("destroyingBlock() Start");
+        yield return new WaitForSeconds(1f);
+        GameMaster.destructionSemaphore = false;
+        Debug.Log("destroyingBlock() End");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (newGemX != 0.1f) {
+            StartCoroutine(destroyingBlock());
+            int random = Random.Range(1, 6);
+            if (random == 1)
+            {
+                Instantiate(BlueGem, new Vector3(newGemX, (float)(4.4)), new Quaternion());
+            }
+            if (random == 2)
+            {
+                Instantiate(RedGem, new Vector3(newGemX, (float)(4.4)), new Quaternion());
+            }
+            if (random == 3)
+            {
+                Instantiate(YellowGem, new Vector3(newGemX, (float)(4.4)), new Quaternion());
+            }
+            if (random == 4)
+            {
+                Instantiate(GreenGem, new Vector3(newGemX, (float)(4.4)), new Quaternion());
+            }
+            if (random > 4)
+            {
+                Instantiate(PinkGem, new Vector3(newGemX, (float)(4.4)), new Quaternion());
+            }
+            newGemX = 0.1f;
+        }
+	}
+}
---
+++ Assets/Scripts/GemControl.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemControl : MonoBehaviour {
+
+    public string color;
+    public List<GemControl> destructionList;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+    void Update () {
+       if (GameMaster.destructionSemaphore == false) {
+            destructionList = new List<GemControl>();
+            destructionList.AddRange(checkMatches("UP"));
+            destructionList.AddRange(checkMatches("DOWN"));
+            destructionList.AddRange(checkMatches("LEFT"));
+            destructionList.AddRange(checkMatches("RIGHT"));
+            for (int i = 0; i < destructionList.Count; i++) {
+                if (GameMaster.destructionSemaphore == false)
+                {
+                    /*
+                    GameMaster.destructionSemaphore = true;
+                    GameMaster.newGemX = this.transform.position.x;
+                    Object.DestroyImmediate(destructionList[0].gameObject);
+                    */
+                }
+            }
+        }
+    }
+
+    public List<GemControl> checkMatches(string direction)
+    {
+        GemControl gemControl;
+        List<GemControl> destructionList = new List<GemControl>();
+        List<GemControl> temp = new List<GemControl>();
+        float x, y;
+        if (direction.Equals("UP"))
+        {
+            x = 0;
+            y = 1;
+        }
+        else if (direction.Equals("DOWN"))
+        {
+            x = 0;
+            y = -1;
+        }
+        else if (direction.Equals("LEFT"))
+        {
+            x = -1;
+            y = 0;
+        }
+        else if (direction.Equals("RIGHT"))
+        {
+            x = 1;
+            y = 0;
+        }
+        else {
+            return null;
+        }
+        Vector3 extent = this.GetComponent<Collider2D>().bounds.extents;
+        Vector3 center = this.GetComponent<Collider2D>().bounds.center;
+        GameObject hit;
+        if (Physics2D.Raycast(new Vector2(center.x + extent.x * x, center.y + extent.y * y), new Vector2(x, y)).rigidbody != null)
+        {
+            hit = Physics2D.Raycast(new Vector2(center.x + extent.x * x, center.y + extent.y * y), new Vector2(x, y)).rigidbody.gameObject;
+            if ((gemControl = hit.GetComponent<GemControl>()) != null)
+            {
+                if (gemControl.color.Equals(this.color))
+                {
+                    Debug.Log(gemControl.color + " " + this.color + " " + (gemControl.color.Equals(this.color)));
+                    destructionList.Add(gemControl);
+                    /*if ((temp = gemControl.checkMatches(direction)) != null)
+                    {
+                        destructionList.AddRange(temp);
+                        temp = null;
+                    }*/
+                }
+            }
+        }
+        
+        return destructionList;
+    }
+
+    void OnMouseDown() {
+        if(GameMaster.destructionSemaphore == false) {
+            GameMaster.destructionSemaphore = true;
+            GameMaster.newGemX = this.transform.position.x;
+            Object.DestroyImmediate(gameObject);
+        }
+    }
+
+}
